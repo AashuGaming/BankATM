4d0b06b01a167976805ac8808ec13e9f2e16ccca
AashuGaming <77149352+AashuGaming@users.noreply.github.com>
2021-09-10 09:10:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function App() {
+  render(){
+    return(
+      constructor{
+        super
+      }
+    )
+  }
+}
+  
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+});
